46a4074386c22fd007272e017107333f236442da
curiositypaths <curiositypaths@users.noreply.github.com>
2017-06-09 10:59:52 -0400

    Remove references to Handlebars.js

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-function handlebarsSetup() {
-  //put any handlebars setup in here
-  Handlebars.registerPartial("userDetails", $("#user-details-partial").html())
-}
-
 $(document).ready(function (){
-  handlebarsSetup()
+  // Write your code here
 });
